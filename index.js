3751ea41608e8f0f4ce2663ba24c7f4660ece741
NIRAROBOT <romeromusic@gmail.com>
2026-05-08 20:46:23 -0400

    Update index.js May 8/26 8:46 PM

--- index.js
+++ index.js
@@ -150,7 +150,7 @@ app.post("/chat", async (req, res) => {
         method: "POST",
       headers: {
   "Content-Type": "application/json",
-"Authorization": `Bearer ${OPENAI_API_KEY}`
+  "Authorization": `Bearer ${OPENAI_API_KEY}`
 },
         body: JSON.stringify({
           model: "gpt-4o-mini",
